50189af94af89e2336be08f37cec0271c4e941e6
BiaoWu <biaoliu_2007@163.com>
2015-12-31 15:41:45 +0800

    remove dependencies ViewScrollChecker change

--- pulltorefresh/src/main/java/com/biao/pulltorefresh/utils/ViewScrollChecker.java
+++ pulltorefresh/src/main/java/com/biao/pulltorefresh/utils/ViewScrollChecker.java
@@ -1,11 +1,6 @@
 package com.biao.pulltorefresh.utils;
 
 import android.os.Build;
-import android.support.v4.view.ViewCompat;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
@@ -36,24 +31,37 @@ public class ViewScrollChecker {
     }
 
     public static boolean canViewScrollVertically(View view, int direction) {
-        if (DEBUG_SCROLL_CHECK)
-            L.e(TAG, "view = %s , direction is %s", view, (direction > 0 ? "up" : "down"));
+        boolean result = false;
         if (Build.VERSION.SDK_INT < 14) {
             if (view instanceof AbsListView) {//list view etc.
-                return performAbsListView((AbsListView) view, direction);
-            } else if (view instanceof RecyclerView) {
-                return performRecyclerView((RecyclerView) view, direction);
+                result = performAbsListView((AbsListView) view, direction);
             } else {
-                if (DEBUG_SCROLL_CHECK)
-                    L.e(TAG, "this type is not perform!!");
-                return ViewCompat.canScrollVertically(view, -1) || view.getScrollY() > 0;
+                try {//i known it's a bad way!
+                    Class<? extends View> viewClass = view.getClass();
+                    final int offset = (int) viewClass.getDeclaredMethod("computeVerticalScrollOffset").invoke(view);
+                    final int range = (int) viewClass.getDeclaredMethod("computeVerticalScrollRange").invoke(view)
+                            - (int) viewClass.getDeclaredMethod("computeVerticalScrollExtent").invoke(view);
+                    if (range == 0) return false;
+                    if (direction < 0) {
+                        result = (offset > 0);
+                    } else {
+                        result = offset < range - 1;
+                    }
+                } catch (Exception e) {
+                    if (DEBUG_SCROLL_CHECK)
+                        L.e(TAG, "no such method!!");
+                    result = view.getScrollY() > 0;
+                }
             }
         } else {
-            boolean canScrollVertically = view.canScrollVertically(direction);
-            if (DEBUG_SCROLL_CHECK)
-                L.e(TAG, "canViewScrollVertically = %s", canScrollVertically);
-            return canScrollVertically;
+            result = view.canScrollVertically(direction);
         }
+        if (DEBUG_SCROLL_CHECK)
+            L.e(TAG, "view = %s , direction is %s, canViewScrollVertically = %s",
+                    view,
+                    (direction > 0 ? "up" : "down"),
+                    result);
+        return result;
     }
 
     private static boolean performAbsListView(AbsListView view, int direction) {
@@ -80,87 +88,5 @@ public class ViewScrollChecker {
         if (DEBUG_SCROLL_CHECK)
             L.e(TAG, "AbsListView cannot scroll vertically or childCount is 0!!");
         return false;
-    }
-
-    private static boolean performRecyclerView(RecyclerView view, int direction) {
-        RecyclerView.LayoutManager layoutManager = view.getLayoutManager();
-        int childCount = view.getAdapter().getItemCount();
-        boolean canScrollVertically = layoutManager.canScrollVertically();
-        if (DEBUG_SCROLL_CHECK) {
-            L.e(TAG, "recyclerView canScrollVertically = %s, childCount = %s!!", canScrollVertically, childCount);
-        }
-        if (canScrollVertically && childCount > 0) {
-            switch (direction) {
-                case DIRECTION_DOWN:
-                    int firstPosition = -1;
-                    if (layoutManager instanceof GridLayoutManager) {
-                        firstPosition = ((GridLayoutManager) layoutManager).findFirstVisibleItemPosition();
-                    } else if (layoutManager instanceof LinearLayoutManager) {
-                        firstPosition = ((LinearLayoutManager) layoutManager).findFirstVisibleItemPosition();
-                    } else if (layoutManager instanceof StaggeredGridLayoutManager) {
-                        int[] firstPositions = new int[((StaggeredGridLayoutManager) layoutManager).getSpanCount()];
-                        ((StaggeredGridLayoutManager) layoutManager).findFirstVisibleItemPositions(firstPositions);
-                        firstPosition = findMin(firstPositions);
-                    }
-                    if (DEBUG_SCROLL_CHECK) {
-                        L.e(TAG, "firstPosition = %s", firstPosition);
-                    }
-                    if (firstPosition == 0) {
-                        ViewGroup.MarginLayoutParams lp = (ViewGroup.MarginLayoutParams) view.getLayoutParams();
-                        int firstItemTop = view.findViewHolderForAdapterPosition(firstPosition).itemView.getTop();
-                        int recyclerViewTop = view.getTop() + view.getPaddingTop() - lp.topMargin;
-                        if (DEBUG_SCROLL_CHECK)
-                            L.e(TAG, "firstItemTop=%s,recyclerViewTop=%s", firstItemTop, recyclerViewTop);
-                        return firstItemTop < recyclerViewTop;
-                    } else {
-                        return true;
-                    }
-                case DIRECTION_UP:
-                    int lastPosition = -1;
-                    if (layoutManager instanceof GridLayoutManager) {
-                        lastPosition = ((GridLayoutManager) layoutManager).findLastVisibleItemPosition();
-                    } else if (layoutManager instanceof LinearLayoutManager) {
-                        lastPosition = ((LinearLayoutManager) layoutManager).findLastVisibleItemPosition();
-                    } else if (layoutManager instanceof StaggeredGridLayoutManager) {
-                        int[] lastPositions = new int[((StaggeredGridLayoutManager) layoutManager).getSpanCount()];
-                        ((StaggeredGridLayoutManager) layoutManager).findLastVisibleItemPositions(lastPositions);
-                        lastPosition = findMax(lastPositions);
-                    }
-                    if (DEBUG_SCROLL_CHECK) {
-                        L.e(TAG, "lastPosition = %s", lastPosition);
-                    }
-
-                    if (lastPosition == childCount - 1) {
-                        int lastItemBottom = view.findViewHolderForAdapterPosition(lastPosition).itemView.getBottom();
-                        int recyclerViewBottom = view.getBottom() - view.getPaddingBottom();
-                        if (DEBUG_SCROLL_CHECK)
-                            L.e(TAG, "lastItemBottom=%s,recyclerViewBottom=%s", lastItemBottom, recyclerViewBottom);
-                        return lastItemBottom > recyclerViewBottom;
-                    } else {
-                        return true;
-                    }
-            }
-        }
-        return false;
-    }
-
-    private static int findMax(int[] lastPositions) {
-        int max = lastPositions[0];
-        for (int value : lastPositions) {
-            if (value > max) {
-                max = value;
-            }
-        }
-        return max;
-    }
-
-    private static int findMin(int[] firstPositions) {
-        int min = firstPositions[0];
-        for (int value : firstPositions) {
-            if (value > min) {
-                min = value;
-            }
-        }
-        return min;
     }
 }
